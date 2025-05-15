92f3b37ec0f7242184e8d485c4444af6f69861de
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-05-15 15:27:18 +0100

    my work

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Services from "./components/Services";
+import Work from "./components/Work";
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
     <Header/>
     <About/>
     <Services/>
+    <Work/>
     </>
   );
 }
